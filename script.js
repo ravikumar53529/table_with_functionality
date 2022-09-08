b1d25dd0e79f853bd0e3842fd05993888ef0fbc2
ravikumar53529 <68583109+ravikumar53529@users.noreply.github.com>
2022-09-08 16:25:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function add_Newrowtop(){
+  
+    document.getElementById("firstrow").style.display="table-row";
+    const arr=[2,3,4];
+    document.getElementById("sln2").innerHTML="2";
+    document.getElementById("sln3").innerHTML="3";
+    document.getElementById("sln4").innerHTML="4";
+    document.getElementById("sln5").innerHTML="5";
+    document.getElementById("sln6").innerHTML="6";
+
+    
+}
+function add_Newrowbottom(){
+ 
+    document.getElementById("lastrow").style.display="table-row";
+}
